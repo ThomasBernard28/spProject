ec67c717984456ed7297fe95408f1fbfec9b02d3
Thomas Bernard <thomasbernard@MacBook-Pro-de-Thomas.local>
2022-11-23 13:53:55 +0100

    add imports in utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
 #Class containing methods
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+import scipy.io.wavfile as wf
+from collections import deque
+import scipy.signal as sc
+from scipy.optimize import root
+import pyaudio
